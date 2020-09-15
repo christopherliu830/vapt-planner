2c3d32f63322a062b055881459ec8b7b19c9b420
Christopher Liu <christopherliu830@gmail.com>
2020-09-15 18:09:42 -0400

    palettes now have fixed width

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import { FileSelector, ImageDisplay, Toolbar, FurniturePalette}  from './components';
 import WallTool from './tools/WallTool';
 import FurnitureTool from './tools/FurnitureTool';
 import { connect } from 'react-redux';
 import { updateCanvasAction, toolSelectedAction } from './redux/actions';
 import { fabric } from 'fabric';
+import './App.css';
 
 function App({updateCanvas, currentTool}) {
   const [canvas, setCanvas] = useState(new fabric.Canvas());
@@ -39,19 +39,21 @@ function App({updateCanvas, currentTool}) {
 
   return (
     <div className="App d-flex flex-column h-100">
-      <header className="App-header navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <header className="App-header navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow-r">
         Select: <FileSelector canvas={canvas}/>
       </header>
-      <div className="container-fluid pb-0 h-100" style={{overflow:"hidden"}}>
-        <div className="row h-100">
-          <div class="palette col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div className="container-fluid p-0 h-100">
+        <div className="row h-100 w-100 d-flex p-0 m-0">
+          {/* <div class="palette col-lg-1 d-md-block bg-light sidebar shadow"> */}
+          <div class="palette fixed-left shadow">
             <h3>Tools</h3>
             <Toolbar/>
           </div>
-          <div class="col-md-6 col-lg-8 px-0" role="main">
+          <div class="col main align-self-stretch px-0" role="main">
             <ImageDisplay canvas={canvas}/>
           </div>
-          <div class="palette col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+          {/* <div class="palette col-lg-1 d-md-block bg-light sidebar collapse shadow"> */}
+          <div class="palette fixed-right p-0 m-0 shadow">
             <h3>Furniture</h3>
             <FurniturePalette/>
           </div>
@@ -60,7 +62,7 @@ function App({updateCanvas, currentTool}) {
       </div>
       <footer className="footer my-0 py-0">
         <div className="container">
-          footer
+          <span class="text-muted">VirtualAPT</span>
         </div>
       </footer>
     </div>
